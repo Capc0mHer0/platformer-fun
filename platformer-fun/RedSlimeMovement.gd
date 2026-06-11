62ebe315ce1929a1412e3fd3d3bdb6201ceb2b05
Capc0mHer0 <noahc2001@hotmail.com>
2026-06-11 01:41:40 -0400

    added red slime and gave it similiar movement script from green slime

---
+++ platformer-fun/RedSlimeMovement.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+class_name RedSlime
+
+@export var gravity: int = 50
+
+func _physics_process(_delta: float) -> void:
+	if is_on_floor():
+		position.x -= 1
+	if not is_on_floor():
+		velocity.y = gravity
+	move_and_slide()

b54aecf952b44db4e6504b2955d5b7354cd37252
MiDe-S <dmasiak7@gmail.com>
2026-07-14 12:51:25 -0400

    add edge detection for red and green slimes

--- platformer-fun/enemies/GreenSlimeMovement.gd
+++ platformer-fun/enemies/GreenSlimeMovement.gd
@@ -3,8 +3,7 @@ class_name GreenSlime
 
 func _physics_process(delta: float) -> void:
 	if is_on_wall():
-		$GreenSlime.flip_h = !$GreenSlime.flip_h
-		speed *= -1
+		flip_direction()
 	if is_on_floor():
 		velocity.x = speed
 	if not is_on_floor():
@@ -14,3 +13,7 @@ func _physics_process(delta: float) -> void:
 	
 func _on_slime_hurt_box_body_entered(body: Node2D) -> void:
 	kill_Slime(body)
+	
+func flip_direction() -> void:
+	$GreenSlime.flip_h = !$GreenSlime.flip_h
+	speed *= -1
--- platformer-fun/enemies/RedSlimeMovement.gd
+++ platformer-fun/enemies/RedSlimeMovement.gd
@@ -3,8 +3,7 @@ class_name RedSlime
 
 func _physics_process(_delta: float) -> void:
 	if is_on_wall():
-		$RedSlime.flip_h = !$RedSlime.flip_h
-		speed *= -1
+		flip_direction()
 	if is_on_floor():
 		velocity.x = speed
 	if not is_on_floor():
@@ -14,3 +13,7 @@ func _physics_process(_delta: float) -> void:
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	kill_Slime(body)
+
+func flip_direction() -> void:
+	$RedSlime.flip_h = !$RedSlime.flip_h
+	speed *= -1
